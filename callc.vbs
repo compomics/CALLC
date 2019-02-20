e31e46f1b424868407922f360bcbf52a7a2baec3
robbin bouwmeester <robbin.bouwmeester@ugent.be>
2019-02-20 11:24:41 +0100

    Switched to yml requirements
    Switched to yml requirements file.

--- callc.vbs
+++ callc.vbs
@@ -1,4 +1,4 @@
 Set oShell = CreateObject ("Wscript.Shell") 
 Dim strArgs
-strArgs = "cmd /c rt/callc.bat"
+strArgs = "cmd /c callc.bat"
 oShell.Run strArgs, 0, false
--- install/install.bat
+++ install/install.bat
@@ -1,6 +1,1 @@
-conda create --yes --name callc & ^
-conda activate callc & ^
-conda install --yes --file requirements.txt & ^
-conda install --yes -c rdkit rdkit & ^
-conda install --yes -c r r & ^
-pip install PyQt5
+conda env create -f environment_callc.yml
